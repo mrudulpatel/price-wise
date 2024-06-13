e3466cc6c51343197dd3c7c0d037d6f3d6b625e5
Mrudul Patel <mrudulpatel04@gmail.com>
2024-06-13 12:49:12 +0530

    chore: Update cron route with new constants and fix variable name

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -10,6 +10,9 @@ import {
 } from "@/lib/scraper/utils";
 import { NextResponse } from "next/server";
 
+export const maxDuration = 300;
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 export async function GET() {
   try {
     connectToDB();
@@ -33,7 +36,7 @@ export async function GET() {
           averagePrice: getAveragePrice(updatedPriceHistory),
         };
         const updatedProduct = await Product.findOneAndUpdate(
-          { url: scrapedProduct.url },
+          { url: product.url },
           product
         );
 
